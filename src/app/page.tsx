aa73767a3ff32bed7838a89d6f155400ecc7dc9b
Viren Chauhan <111180207+viren-chauhan@users.noreply.github.com>
2024-10-24 14:55:35 +0530

    UI Done!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const arr = [1, 2, 3, 4, 5, 6]
+const arr = [1, 2, 3, 4, 5, 6];
 
 export default function Home() {
   return (
@@ -11,56 +11,104 @@ export default function Home() {
         <div>
           <div className="flex flex-row justify-between gap-3 pt-4">
             <div className="h-40 rounded-md border bg-[#2e334d] flex-1 p-3">
-              <textarea name="prompt" id="prompt" className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none" placeholder="Enter your imagination..."></textarea>
+              <textarea
+                name="prompt"
+                id="prompt"
+                className="bg-transparent w-full resize-none text-white px-3 py-2 h-full focus:outline-none"
+                placeholder="Enter your imagination..."
+              ></textarea>
             </div>
             <div className="w-1/4 h-40 rounded-md border bg-[#2e334d] p-5 flex flex-col justify-between">
               <div className="flex-row flex justify-between">
-                <label htmlFor="imgSize" className="text-white">Image Size</label>
-                <input type="text" id="imgSize" className="rounded-md border bg-transparent" />
+                <label htmlFor="imgSize" className="text-white">
+                  Image Size
+                </label>
+                <input
+                  type="text"
+                  id="imgSize"
+                  className="rounded-md border bg-transparent text-white px-2 focus:outline-none"
+                />
               </div>
               <div className="flex-row flex justify-between">
-                <label htmlFor="scale" className="text-white">Scale</label>
-                <input type="text" id="scale" className="rounded-md border bg-transparent" />
+                <label htmlFor="scale" className="text-white">
+                  Scale
+                </label>
+                <input
+                  type="text"
+                  id="scale"
+                  className="rounded-md border bg-transparent text-white px-2 focus:outline-none"
+                />
               </div>
               <div className="flex-row flex justify-between">
-                <label htmlFor="steps" className="text-white">Steps</label>
-                <input type="text" id="steps" className="rounded-md border bg-transparent" />
+                <label htmlFor="steps" className="text-white">
+                  Steps
+                </label>
+                <input
+                  type="text"
+                  id="steps"
+                  className="rounded-md border bg-transparent text-white px-2 focus:outline-none"
+                />
               </div>
             </div>
           </div>
 
           <div className="flex flex-row justify-between gap-3 pt-4">
             <div className="flex-1">
-              <h1 className="text-white font-semibold rounded-md py-2">Automatic Negative Prompt</h1>
+              <h1 className="text-white font-semibold rounded-md py-2">
+                Automatic Negative Prompt
+              </h1>
               <div className="h-40 rounded-md border bg-[#2e334d] flex-1 p-3">
-                <textarea name="prompt" id="prompt" className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none" placeholder="Enter your imagination..."></textarea>
+                <textarea
+                  name="prompt"
+                  id="prompt"
+                  className="bg-transparent w-full resize-none text-white px-3 py-2 h-full focus:outline-none"
+                  placeholder="Enter your imagination..."
+                ></textarea>
               </div>
             </div>
             <div className="flex-1">
-              <h1 className="text-white font-semibold rounded-md py-2">Negative Prompt</h1>
+              <h1 className="text-white font-semibold rounded-md py-2">
+                Negative Prompt
+              </h1>
               <div className="h-40 rounded-md border bg-[#2e334d] flex-1 p-3">
-                <textarea name="prompt" id="prompt" className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none" placeholder="Enter your negative prompt..."></textarea>
+                <textarea
+                  name="prompt"
+                  id="prompt"
+                  className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none h-full"
+                  placeholder="Enter your negative prompt..."
+                ></textarea>
               </div>
             </div>
           </div>
-          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md">Generate Image</button>
+          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md cursor-pointer">
+            Generate Image
+          </button>
         </div>
-
       </div>
       <div className="flex-grow bg-[#2c2d44] rounded-md p-3">
         <p className="text-white text-lg font-semibold">Generate Result</p>
         <div className="h-48 rounded-md border flex gap-5 overflow-x-auto p-3">
-          {
-            arr.map((item, index) => (
-              <Image alt="Garden" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPnYiXnTnaEBkJtVwT9SI16EOBb9JjcjU12A&s"} height={100} width={300} />
-            ))
-          }
+          {arr.map((item, index) => (
+            <Image
+              key={index}
+              alt="Garden"
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPnYiXnTnaEBkJtVwT9SI16EOBb9JjcjU12A&s"
+              }
+              height={100}
+              width={300}
+            />
+          ))}
         </div>
         <div className="gap-3 flex">
-          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md min-w-40">Save</button>
-          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md max-w-40">Generate Video</button>
+          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md min-w-40 cursor-pointer">
+            Save
+          </button>
+          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md max-w-40 cursor-pointer">
+            Generate Video
+          </button>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
