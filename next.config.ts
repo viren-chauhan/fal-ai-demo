e00b087b2c8ca4cbe7202b6c9c8d26f3451a3fcb
Jay R <samier.sompura@gmail.com>
2024-10-24 14:12:03 +0530

    demo created!

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'encrypted-tbn0.gstatic.com',
+  //       port: '',
+  //       pathname: '/account123/**',
+  //     },
+  //   ],
+};
+
+module.exports = {
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com"],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
 import Image from "next/image";
 
+const arr = [1, 2, 3, 4, 5, 6]
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen bg-[#111419] flex flex-col gap-5 p-4">
+      <h1 className="text-white text-2xl font-semibold">General Image</h1>
+      <div className="flex-grow bg-[#2c2d44] rounded-md p-3">
+        <p className="text-white text-lg font-semibold">Prompt</p>
+        <div>
+          <div className="flex flex-row justify-between gap-3 pt-4">
+            <div className="h-40 rounded-md border bg-[#2e334d] flex-1 p-3">
+              <textarea name="prompt" id="prompt" className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none" placeholder="Enter your imagination..."></textarea>
+            </div>
+            <div className="w-1/4 h-40 rounded-md border bg-[#2e334d] p-5 flex flex-col justify-between">
+              <div className="flex-row flex justify-between">
+                <label htmlFor="imgSize" className="text-white">Image Size</label>
+                <input type="text" id="imgSize" className="rounded-md border bg-transparent" />
+              </div>
+              <div className="flex-row flex justify-between">
+                <label htmlFor="scale" className="text-white">Scale</label>
+                <input type="text" id="scale" className="rounded-md border bg-transparent" />
+              </div>
+              <div className="flex-row flex justify-between">
+                <label htmlFor="steps" className="text-white">Steps</label>
+                <input type="text" id="steps" className="rounded-md border bg-transparent" />
+              </div>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex flex-row justify-between gap-3 pt-4">
+            <div className="flex-1">
+              <h1 className="text-white font-semibold rounded-md py-2">Automatic Negative Prompt</h1>
+              <div className="h-40 rounded-md border bg-[#2e334d] flex-1 p-3">
+                <textarea name="prompt" id="prompt" className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none" placeholder="Enter your imagination..."></textarea>
+              </div>
+            </div>
+            <div className="flex-1">
+              <h1 className="text-white font-semibold rounded-md py-2">Negative Prompt</h1>
+              <div className="h-40 rounded-md border bg-[#2e334d] flex-1 p-3">
+                <textarea name="prompt" id="prompt" className="bg-transparent w-full resize-none text-white px-3 py-2 focus:outline-none" placeholder="Enter your negative prompt..."></textarea>
+              </div>
+            </div>
+          </div>
+          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md">Generate Image</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+      </div>
+      <div className="flex-grow bg-[#2c2d44] rounded-md p-3">
+        <p className="text-white text-lg font-semibold">Generate Result</p>
+        <div className="h-48 rounded-md border flex gap-5 overflow-x-auto p-3">
+          {
+            arr.map((item, index) => (
+              <Image alt="Garden" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPnYiXnTnaEBkJtVwT9SI16EOBb9JjcjU12A&s"} height={100} width={300} />
+            ))
+          }
+        </div>
+        <div className="gap-3 flex">
+          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md min-w-40">Save</button>
+          <button className="mt-3 p-2 bg-[#b747d0] text-white font-semibold rounded-md max-w-40">Generate Video</button>
+        </div>
+      </div>
+    </div >
   );
 }
