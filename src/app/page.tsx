1c2a7f4249558ec2d8d7120b175de3640534df5b
Jay R <samier.sompura@gmail.com>
2024-10-24 18:25:14 +0530

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,21 +39,19 @@ export default function Home() {
     const textarea = textareaRef.current;
 
     if (textarea) {
-      // Get current cursor position
       const start = textarea?.selectionStart;
       const end = textarea?.selectionEnd;
-
-      // Insert the selected path at the current cursor position
       const updatedText = textValue.slice(0, start) + path + textValue.slice(end);
       setTextValue(updatedText);
-
-      // Set cursor position after the inserted text
       setTimeout(() => {
         textarea?.focus();
         textarea?.setSelectionRange(start + path.length, start + path.length);
       }, 0);
     }
   };
+  const [scale, setScale] = React.useState<number>(1.2);
+  const [noOfImages, setNoOfImages] = React.useState<number>(4);
+  const [steps, setSteps] = React.useState<number>(1);
 
   return (
     <div className="min-h-screen bg-[#111419] flex flex-col gap-5 p-4">
@@ -76,53 +74,95 @@ export default function Home() {
               </div>
               <div className="w-2/4 h-44 rounded-md border bg-[#2e334d] p-5 flex flex-col justify-between">
                 <div className="flex-row flex justify-between">
-                  <label htmlFor="imgSize" className="text-white">
-                    No of images
+                  <label htmlFor="noOfImages" className="text-white">
+                    No. of images
                   </label>
-                  <input
-                    type="text"
-                    id="imgSize"
-                    className="rounded-md border bg-transparent text-white px-2 focus:outline-none"
-                  />
+                  <div className="w-8/12 flex justify-center gap-2">
+                    <input
+                      type="range"
+                      min={0}
+                      max={4}
+                      value={noOfImages}
+                      id="noOfImages"
+                      step={1}
+                      className="w-11/12"
+                      onChange={(e: any) => setNoOfImages(Number(e.target.value))}
+                    />
+                    <span style={{ borderRadius: 8, height: 25, width: 60, backgroundColor: 'white' }}>
+                      <input onChange={(e: any) => Number(e?.target?.value) < 5 && setNoOfImages(Math.round(Number(e?.target?.value)) || 0)} className="w-full bg-transparent focus:outline-none text-black text-center" value={noOfImages} type="number" max={4} min={0} />
+                    </span>
+                  </div>
                 </div>
                 <div className="flex-row flex justify-between">
                   <label htmlFor="imgSize" className="text-white">
                     Image Size
                   </label>
-                  <TextInput
-                    type={"text"}
-                    id={"imgSize"}
-                    style={
-                      "rounded-md border bg-transparent text-white px-2 focus:outline-none"
-                    }
-                    placeholder={""}
-                  />
+                  <select
+                    name="imgSize"
+                    id="imgSize"
+                    onChange={(e) => console.log(e.target.value)}
+                    className="rounded-md border bg-transparent text-white w-8/12 focus:outline-none"
+                  >
+                    <option value="square" className="text-black">
+                      Square
+                    </option>
+                    <option value="squarehd" className="text-black">
+                      Square HD
+                    </option>
+                    <option value="portrait_3_4" className="text-black">
+                      Portrait 3:4
+                    </option>
+                    <option value="portrait_9_16" className="text-black">
+                      Portrait 9:16
+                    </option>
+                    <option value="landscape_4_3" className="text-black">
+                      Landscape 4:3
+                    </option>
+                    <option value="landscape_16_9" className="text-black">
+                      Landscape 16:9
+                    </option>
+                  </select>
                 </div>
                 <div className="flex-row flex justify-between">
                   <label htmlFor="scale" className="text-white">
                     Scale
                   </label>
-                  <TextInput
-                    type={"text"}
-                    id={"scale"}
-                    style={
-                      "rounded-md border bg-transparent text-white px-2 focus:outline-none"
-                    }
-                  />
+                  <div className="w-8/12 flex justify-center gap-2">
+                    <input
+                      type="range"
+                      min={0.0}
+                      max={4.0}
+                      value={scale}
+                      id="scale"
+                      step={0.1}
+                      className="w-11/12"
+                      onChange={(e: any) => setScale(Number(e.target.value))}
+                    />
+                    <span style={{ borderRadius: 8, height: 25, width: 60, backgroundColor: 'white' }}>
+                      <input onChange={(e: any) => Number(e?.target?.value) < 5 && setScale(Number(e?.target?.value) || 0)} className="w-full bg-transparent focus:outline-none text-black text-center" value={scale} type="number" max={4} min={0} />
+                    </span>
+                  </div>
                 </div>
                 <div className="flex-row flex justify-between">
                   <label htmlFor="steps" className="text-white">
                     Steps
                   </label>
-                  <TextInput
-                    type={"text"}
-                    id={"steps"}
-                    style={
-                      "rounded-md border bg-transparent text-white px-2 focus:outline-none"
-                    }
-                  />
+                  <div className="w-8/12 flex justify-center gap-2">
+                    <input
+                      type="range"
+                      min={0}
+                      max={50}
+                      value={steps}
+                      id="steps"
+                      step={1}
+                      className="w-11/12"
+                      onChange={(e: any) => setSteps(Number(e.target.value))}
+                    />
+                    <span style={{ borderRadius: 8, height: 25, width: 60, backgroundColor: 'white' }}>
+                      <input onChange={(e: any) => Number(e?.target?.value) < 51 && setSteps((Math.round(Number(e?.target?.value))) || 0)} className="w-full bg-transparent focus:outline-none text-black text-center" value={steps} type="number" max={4} min={0} />
+                    </span>
+                  </div>
                 </div>
-
               </div>
             </div>
 
